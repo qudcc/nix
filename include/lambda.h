30734b0cb29698d6cde9c663687dfb0d8afdc5e2
User Qu <qu@freebsd.amd64>
2018-04-24 07:06:36 +0800

    add lambda.h

---
+++ include/lambda.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) Qu. All right reserved.
+ */
+
+
+#ifndef NIX_LAMBDA_H
+#define NIX_LAMBDA_H
+
+
+/* lambda macro */
+/* C language statement embedded expression extension definition */
+#define $(return_type, function_body) ({ return_type $ function_body $; })
+
+
+#endif /* NIX_LAMBDA_H */
