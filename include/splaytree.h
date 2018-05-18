dcf561a394d9905e6f3140cd13be27aff6ea1d84
qudcc <38711676+qudcc@users.noreply.github.com>
2018-05-18 11:32:28 +0800

    Update splaytree.h

--- include/splaytree.h
+++ include/splaytree.h
@@ -16,15 +16,15 @@ typedef struct {
 } nix_splay_tree_t;
 
 void nix_splay_tree_init(nix_splay_tree_t *tree,
-        int (*pred)(const nix_splay_tree_node_t *, intptr_t));
+    int (*pred)(const nix_splay_tree_node_t *, intptr_t));
 int nix_splay_tree_insert(nix_splay_tree_t *t, intptr_t key,
-        nix_splay_tree_node_t *(*alloc)(intptr_t key));
+    nix_splay_tree_node_t *(*alloc)(intptr_t key));
 nix_splay_tree_node_t *nix_splay_tree_erase(nix_splay_tree_t *t,
-        intptr_t key);
+    intptr_t key);
 nix_splay_tree_node_t *nix_splay_tree_find(nix_splay_tree_t *t,
-        intptr_t key);
+    intptr_t key);
 void nix_splay_tree_each(const nix_splay_tree_node_t *node,
-        void (*cb)(const nix_splay_tree_node_t *node));
+    void (*cb)(const nix_splay_tree_node_t *node));
 
 
 #endif /* NIX_SPLAYTREE_H */
