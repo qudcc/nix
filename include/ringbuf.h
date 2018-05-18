46b885aa921acb3a62f5d295722b311f3a8785f0
qudcc <38711676+qudcc@users.noreply.github.com>
2018-05-18 16:33:34 +0800

    Update ringbuf.h

--- include/ringbuf.h
+++ include/ringbuf.h
@@ -13,11 +13,11 @@ typedef struct {
 } nix_ring_buf_t;
 
 #define nix_ring_buf_init(b, sz) \
-	(b)->size = sz, (b)->read = (b)->write = 0
+    (b)->size = sz, (b)->read = (b)->write = 0
 #define nix_ring_buf_empty(b) ((b)->read == (b)->write)
 #define nix_ring_buf_full(b) ((b)->read == (((b)->write + 1) % (b)->size))
 #define nix_ring_buf_length(b) \
-	(((b)->size + (b)->write - (b)->read) % (b)->size)
+    (((b)->size + (b)->write - (b)->read) % (b)->size)
 #define nix_ring_buf_read(b, len) (b)->read += len, (b)->read %= (b)->size
 #define nix_ring_buf_write(b, len) (b)->write += len, (b)->write %= (b)->size
 
